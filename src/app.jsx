3e9fcddc7018797f3b10d2bf4d295ced3f976641
autumnly1007 <autumnly1007@gmail.com>
2022-02-05 16:17:24 +0900

    Habit Component

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,10 @@
-import './app.css';
+import { getAllByAltText } from "@testing-library/react";
+import "./app.css";
+import Habit from "./components/habit";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+  return <Habit />;
 }
 
 export default App;
